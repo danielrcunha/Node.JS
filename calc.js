cdae46e24abde6a44325388d9cb5867d6273cece
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-05-21 21:21:55 -0300

    commit
    commit

--- calc.js
+++ calc.js
@@ -1,4 +1,10 @@
 /*chamando o modulo*/
 var SomaFunc = require("./soma");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+var DivFunc = require("./div");
 
 console.log(SomaFunc(1, 2));
+console.log(SubFunc(10, 5));
+console.log(MultiFunc(100, 10));
+console.log(DivFunc(100, 2));
