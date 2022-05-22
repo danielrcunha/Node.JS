53659e8428365fb64f26d8dfceb5d54048a3b686
danielrcunha <74882067+danielrcunha@users.noreply.github.com>
2022-05-21 22:47:47 -0300

    commit
    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Seja bem-vindo ao meu app");
+});
+
+app.get("/sobre", function (req, res) {
+    res.send("Minha pagina sobre");
+})
+
+app.get("/blog", function (req, res) {
+    res.send("Bem-vindo ao meu blog!");
+})
+
+app.listen(8080, function () {
+    console.log("Servidor Rodando na URL http://localhost:8080");
+});
